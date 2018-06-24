d36cdd8f2667de835de87ead14674455cb67f20e
vamshikn <vamshikn@upscalesoft.in>
2018-06-24 20:22:19 +0530

    started 24/6/2018

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React , {Component }from 'react';
-
+import './style.css'
 
 class App extends Component{
     render(){
         return(
-            <div>
-               
+            <div className="whole">
+               <img className="pro-pic" src="https://avatars1.githubusercontent.com/u/28837336?s=460&v=4 "></img>
             </div>
         );
     }
