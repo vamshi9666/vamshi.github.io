cbb83d0934e610f0633edd204c5359bb613223da
vamshikn <vamshikn@upscalesoft.in>
2018-06-27 00:19:09 +0530

    tag line and profile picture

--- src/app.js
+++ src/app.js
@@ -5,9 +5,17 @@ class App extends Component{
     render(){
         return(
             <div className="whole">
-               <img className="pro-pic" src="https://avatars1.githubusercontent.com/u/28837336?s=460&v=4 "></img>
+              <div className="head1">
+                <img className="pro-pic" src="https://avatars1.githubusercontent.com/u/28837336?s=460&v=4"></img>
+                <h2 className="" >Vamshi Krishna Nimmakuri</h2>
+              </div>
+              <p className="tag-line">  I used to say JavaSCript everywhere.</p>
+               <p className="tag-line">And now i am saying Rust everywhere </p>
+
             </div>
         );
     }
 }
+
+
 export default App;
