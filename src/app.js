42415151b94e4afab7d45711a3ee353ae710648e
vamshi <vamshikn@upscalesoft.in>
2018-07-07 18:33:52 +0530

    day starts again . so gresh start

--- src/app.js
+++ src/app.js
@@ -1,21 +1,44 @@
 import React , {Component }from 'react';
+import { Route, Switch, Link } from 'react-router-dom'
 import './style.css'
 
+const profile = ()=>(
+  <div className="whole">
+      <div className="head1">
+      <img className="pro-pic" src="https://avatars1.githubusercontent.com/u/28837336?s=460&v=4"></img>
+      <h2 className="" >Vamshi Krishna Nimmakuri</h2>
+    </div>
+  <div className="head2">
+    <p className="tag-line">  I used to say JavaSCript everywhere.</p>
+     <p className="tag-line">And now i am saying Rust everywhere </p>
+  </div>
+
+  </div>
+);
+
+const projects = ()=>(
+    <div>
+      <ul>
+        Projects list
+      </ul>
+    </div>
+);
 class App extends Component{
     render(){
         return(
             <div className="whole">
-              <div className="head1">
-                <img className="pro-pic" src="https://avatars1.githubusercontent.com/u/28837336?s=460&v=4"></img>
-                <h2 className="" >Vamshi Krishna Nimmakuri</h2>
+              <div className="links-con">
+                  <Link className="link" to='/'>
+                    <button>Profile</button>
+                  </Link>
+                  <Link className="link" to='/projects'><button>Projects</button></Link>
               </div>
-            <div className="head2">
-              <p className="tag-line">  I used to say JavaSCript everywhere.</p>
-               <p className="tag-line">And now i am saying Rust everywhere </p>
+              <Switch>
+                <Route  path='/projects' component={projects}/>
+                <Route exact path='/' component={profile}/>
+              </Switch>
             </div>
-
-            </div>
-        );
+          );
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App className="ent"/>,document.getElementById('app'))
+ReactDOM.render(
+  <BrowserRouter>
+    <App className="ent"/>
+  </BrowserRouter>
+  ,document.getElementById('app'))
