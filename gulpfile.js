b2eb3a9d95c20dcee954b8b0a0b300f675970a6e
vamshi9666 <vamshi9666@riseup.net>
2019-01-13 20:35:00 +0530

    init gulp

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+let cleanCSS = require('gulp-clean-css');
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var uglify = require('gulp-uglify');
+
+// Gulp task to minify CSS files
+gulp.task('css', function () {
+  return gulp.src('./css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    // Output
+    .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('js', function() {
+  return gulp.src('./js/*.js')
+    // Minify the file
+    .pipe(uglify())
+    // Output
+    .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('html', function() {
+  return gulp.src(['./*.html'])
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest('./dist'));
+});
